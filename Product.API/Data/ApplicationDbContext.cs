557852d35a15d6a8d30d9e79cf0ef7ee1d505303
Omphemetse Digoamaje <ompheedigoamaje@gmail.com>
2026-02-24 15:55:54 +0200

    Add project files.

---
+++ Product.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YourProjectName.Domain;
+
+namespace YourProjectName.Domain
+{
+	public class ApplicationDbContext : DbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+
+		public DbSet<Product> Products { get; set; }
+	}
+}
